c8e5f52ebf1e18296888844c292b9556ca5056ab
emilevitus <66325272+emilevitus@users.noreply.github.com>
2025-06-10 16:03:39 -0400

    updated

---
+++ dash_app/app.py
@@ -0,0 +1,24 @@
+import dash
+from dash import html , dcc
+
+app  = dash.Dash(__name__, requests_pathname_prefix='/dashboard/')
+
+
+app.layout = html.Div(children=[
+    html.Div([
+        html.A('Home',href='/'),
+    ], style={'marginTop':20}),
+    
+    dcc.Graph(
+        id="exmpl_1",
+        figure={
+            "data":[
+                {"x":[1,2,3] , "y": [6,9,4], "type":"bar", "name":"Example 1" },
+                {"x":[7,2,5] , "y": [3,7,1], "type":"bar", "name":"Example 2" }
+            ]
+        }
+    )
+])
+
+
+server = app.server
