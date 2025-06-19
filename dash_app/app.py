6d5bb05cde3182bca83b492b42d0397e480b878a
emilevitus <66325272+emilevitus@users.noreply.github.com>
2025-06-19 15:57:27 -0400

    Update app.py

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,7 +1,6 @@
 import dash
 from dash import html, dcc
-EXTERNAL_API_URL = 'http://localhost:8000/info'
-# info = 'http://localhost:8000/info' 
+info = 'http://localhost:8000/info' 
 app = dash.Dash(__name__, requests_pathname_prefix='/dashboard/')
 server = app.server
 
@@ -10,11 +9,11 @@ app.layout = html.Div(children=[
         html.A('Home', href='/'),
         html.H2("Bienvenue sur la page de login"),
         html.Div([
-            # html.H3("Aujourd'hui est :"+ info.date +" et maintenant il est :"+info.time),
-            # html.P("Information de Météo"),
-            # html.P("Ville :"+info.city),
-            # html.P("Température :"+info.weather),
-            # html.P("Description :"+ info.description)
+            html.H3("Aujourd'hui est :"+ info.date +" et maintenant il est :"+info.time),
+            html.P("Information de Météo"),
+            html.P("Ville :"+info.city),
+            html.P("Température :"+info.weather),
+            html.P("Description :"+ info.description)
         ], className="info-box", style={"border": "1px solid #ccc", "padding": "10px", "marginTop": "10px"})
     ], style={'marginTop': 20}),
     
